e47c38e6579725b76bc005a28ddfe5446f23b7cc
emiliorevelo <emilio_revelo@protonmail.com>
2019-07-04 12:42:30 -0500

    Update simple-https-server.py

--- simple-https-server.py
+++ simple-https-server.py
@@ -1,14 +1,22 @@
-# taken from http://www.piware.de/2011/01/creating-an-https-server-in-python/
-# generate server.xml with the following command:
-#    openssl req -new -x509 -keyout server.pem -out server.pem -days 365 -nodes
-# run as follows:
-#    python simple-https-server.py
-# then in your browser, visit:
-#    https://localhost:4443
-
+import sys
 import BaseHTTPServer, SimpleHTTPServer
 import ssl
 
-httpd = BaseHTTPServer.HTTPServer(('localhost', 4443), SimpleHTTPServer.SimpleHTTPRequestHandler)
-httpd.socket = ssl.wrap_socket (httpd.socket, certfile='./server.pem', server_side=True)
-httpd.serve_forever()
+port=int(sys.argv[1])
+
+print "Listening on port " + sys.argv[1]+" ..."
+
+
+if len(sys.argv) < 2 :
+ 
+        print "Usage: python "+ sys.argv[0]+" port"
+
+else :
+        httpd = BaseHTTPServer.HTTPServer(('0.0.0.0', port),
+                SimpleHTTPServer.SimpleHTTPRequestHandler)
+
+        httpd.socket = ssl.wrap_socket (httpd.socket,
+                keyfile="empire-priv.key",  # Path to key file
+                certfile='cert.pem', server_side=True)   # Path to cert file
+
+        httpd.serve_forever()
